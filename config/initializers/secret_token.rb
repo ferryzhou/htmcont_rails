e46870d307041fb16d13069d67861a85c3e16a7b
Jin Zhou <ferryzhou@gmail.com>
2011-10-23 20:30:10 -0400

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmcontRails::Application.config.secret_token = 'd8317a9d914e6168b61b48b968f94dbf7cec8b260f3e48f34dbea4a72a3150967fee14e416db40af0c7290939816976fa32728d8efaa907b735cdafff08b29a5'
