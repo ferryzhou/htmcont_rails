45aa8904be0f721d79496e5d922e9919bc69d2ff
Jin Zhou <ferryzhou@gmail.com>
2011-10-29 10:17:00 -0400

    don't do santize

--- app/controllers/readability.rb
+++ app/controllers/readability.rb
@@ -51,7 +51,8 @@ module Readability
       best_candidate = select_best_candidate(candidates)
       article = get_article(candidates, best_candidate)
 
-      cleaned_article = sanitize(article, candidates, options)
+	  cleaned_article = article
+      #cleaned_article = sanitize(article, candidates, options)
       if article.text.strip.length < options[:retry_length]
         if @remove_unlikely_candidates
           @remove_unlikely_candidates = false
